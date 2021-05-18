90dd6faf9d1fcd6da43a805c9a21e8dd29e5d2ea
ankle-code <lucaspasseli.work@gmail.com>
2021-05-18 20:53:37 -0300

    💄 fix: Adjusts stylization props in IconStyled

--- src/components/Icon/style.js
+++ src/components/Icon/style.js
@@ -2,11 +2,17 @@ import styled from "styled-components";
 
 export const IconStyled = styled.img`
   width: 100%;
-  max-width: 32px;
-  margin-top: 30px;
-  padding: 0 5px;
-  padding-bottom: 25px;
+  max-width: 2rem;
+
+  padding: ${({ separator }) =>
+    separator === "Right" && "0.4rem 2rem 0.4rem 0"};
+  padding: ${({ separator }) => separator === "Bottom" && "0 0.4rem 2rem"};
+
+  border-right: ${({ separator }) =>
+    separator === "Right" && "1px solid var(--gray)"};
+
   border-bottom: ${({ separator }) =>
     separator === "Bottom" && "1px solid var(--gray)"};
+
   cursor: ${({ isClickable }) => isClickable && "pointer"};
 `;
