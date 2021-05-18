6578d99a04c713901f865c392ee10dfcf4d4a246
ankle-code <lucaspasseli.work@gmail.com>
2021-05-18 20:05:56 -0300

    ✨ feat: Add SideBarWrap in SideBar component

---
+++ src/components/SideBar/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SideBarWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
