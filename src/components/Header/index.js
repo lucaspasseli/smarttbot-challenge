f65fce27011915ff0bf3a0f03dcc32b8a0394467
ankle-code <lucaspasseli.work@gmail.com>
2021-05-18 20:54:48 -0300

    ✨ feat: Add HeaderWrap component & icons in Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,14 @@
+import Icon from "../Icon";
 import Paper from "../Paper";
+import BarGraph from "../../assets/barGraph.svg";
+import { HeaderWrap } from "./style";
 
-const Header = () => <Paper gridArea="Header" />;
+const Header = () => (
+  <Paper gridArea="Header">
+    <HeaderWrap>
+      <Icon icon={BarGraph} alt="Análise geral" separator="Right" isClickable />
+    </HeaderWrap>
+  </Paper>
+);
 
 export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HeaderWrap = styled.div`
+  display: flex;
+  align-items: center;
+  align-content: center;
+  height: 100%;
+  margin: 0 2rem;
+`;
