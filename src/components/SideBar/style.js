bb4595548ee2bb790a5359289fd420ca1f7fdbf4
ankle-code <lucaspasseli.work@gmail.com>
2021-05-18 20:52:39 -0300

    💄 fix: Adjusts grid and flexbox postioning

--- src/components/SideBar/style.js
+++ src/components/SideBar/style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const SideBarWrap = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  margin-top: 2rem;
+  gap: 2rem;
 `;
--- src/pages/dashboard/style.js
+++ src/pages/dashboard/style.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: calc(100vh - 40px);
   background-color: var(--gray-light);
-  padding: 20px;
+  padding: 1.5rem;
   display: grid;
   grid-template-columns: 4vw auto;
   grid-template-rows: 4vw 2fr 1fr 3fr;
-  gap: 20px 30px;
+  gap: 1.5rem 2rem;
   grid-template-areas:
     "SideBar Header"
     "SideBar Operations"
