fcd7fffa74ea9c3d034097331bad9671ea5a2779
ankle-code <lucaspasseli.work@gmail.com>
2021-05-18 05:32:36 -0300

    ✨ feat: Add root route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import Routes from "./routes";
+
 function App() {
-  return <div></div>;
+  return <Routes />;
 }
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import { Switch, Route } from "react-router-dom";
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/"></Route>
+  </Switch>
+);
+export default Routes;
