5b29dbdf22a56e13f4cec19dd92b54757dff5c2c
ankle-code <lucaspasseli.work@gmail.com>
2021-05-18 20:09:29 -0300

    💄 fix: Adjusts shadow and height in PaperStyled

--- src/components/Paper/style.js
+++ src/components/Paper/style.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 export const PaperStyled = styled.div`
-  height: 100%;
   width: 100%;
   border-radius: 8px;
 
   background-color: var(--white);
-  box-shadow: 0px 0px 1px 0.1px var(--gray);
+  box-shadow: 0px 0px 1px 0.1px rgba(181, 180, 182, 0.85);
 
   grid-area: ${({ gridArea }) => gridArea};
 `;
