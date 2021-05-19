1feff097fdc537950e42caefa22976de7c333e94
ankle-code <lucaspasseli.work@gmail.com>
2021-05-19 14:45:25 -0300

    🎨 refactor: Rename Dashboard styled compo imports

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,18 +1,18 @@
 import SideBar from "../../components/SideBar";
 import Header from "../../components/Header";
 import Operations from "../../components/Operations";
-import { Container } from "./style";
+import * as S from "./style";
 import AddBot from "../../components/AddBot";
 import CardContainer from "../../components/Cards/CardContainer";
 
 const Dashboard = () => (
-  <Container>
+  <S.Container>
     <SideBar />
     <Header />
     <Operations />
     <AddBot />
     <CardContainer />
-  </Container>
+  </S.Container>
 );
 
 export default Dashboard;
