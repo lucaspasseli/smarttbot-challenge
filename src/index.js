7ebc12a532ed29573c93a266ad11e325c9b4e282
ankle-code <lucaspasseli.work@gmail.com>
2021-05-19 16:43:46 -0300

    ✨ feat: Add Redux Store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./styles/globalStyle";
+import { Provider } from "redux-thunk";
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
