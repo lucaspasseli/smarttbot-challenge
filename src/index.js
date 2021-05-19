0d5d7a4dab01b6b08a5faa15da86fa6bc527f15b
ankle-code <lucaspasseli.work@gmail.com>
2021-05-19 16:53:30 -0300

    🐛 fix: Adjuts Provider import in react-redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./styles/globalStyle";
-import { Provider } from "redux-thunk";
+import { Provider } from "react-redux";
 import store from "./store";
 
 ReactDOM.render(
