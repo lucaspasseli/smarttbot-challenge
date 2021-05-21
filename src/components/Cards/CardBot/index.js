0da8be9851f73d6be5281fe93c925c0b6c4c45d2
ankle-code <lucaspasseli.work@gmail.com>
2021-05-21 15:23:10 -0300

    ✨ feat: Add Badge,PaperInfo and Number in Card

--- src/components/Cards/CardBot/index.js
+++ src/components/Cards/CardBot/index.js
@@ -1,5 +1,42 @@
+import * as S from "./style";
 import Paper from "../../Paper";
+import Status from "../../Status";
+import Badge from "../../Badges";
+import Number from "../../Number";
+import PaperInfo from "../../PaperInfo";
 
-const CardBot = ({ number }) => <Paper gridArea={`Card${number}`} />;
+const CardBot = ({ number }) => (
+  <Paper gridArea={`Card${number}`}>
+    <S.Container>
+      <S.Collumn width="40%">
+        <S.Row>
+          <div>
+            <S.Title>Título do Robô</S.Title>
+            <S.Text>#179301</S.Text>
+          </div>
+          <Status isRunning />
+        </S.Row>
+        <S.BadgeWrap>
+          <Badge name="Pessimista" />
+          <Badge name="WIN%" />
+          <Badge name="Price Action" />
+        </S.BadgeWrap>
+        <PaperInfo />
+        <S.Row>
+          <Number description="Saldo diário" isMoney numberValue={-220.0} />
+          <Number description="Trades no dia" alignX="right" numberValue={7} />
+        </S.Row>
+      </S.Collumn>
+      <S.Collumn width="60%">
+        <S.Row>
+          <div>
+            <S.Title>Histórico do dia</S.Title>
+            <S.Text>Última atualização • 16:00</S.Text>
+          </div>
+        </S.Row>
+      </S.Collumn>
+    </S.Container>
+  </Paper>
+);
 
 export default CardBot;
---
+++ src/components/Cards/CardBot/style.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 1.5rem;
+  gap: 1rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Collumn = styled.div`
+  width: ${({ width }) => width || "50%"};
+  height: 80%;
+  gap: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+export const BadgeWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.3rem;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h1`
+  font-weight: 500;
+  margin-bottom: 0.3rem;
+`;
+
+export const Text = styled.p`
+  color: var(--black);
+  opacity: 0.7;
+  font-size: 0.9rem;
+`;
