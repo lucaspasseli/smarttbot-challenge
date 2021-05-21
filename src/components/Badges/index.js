4dc63123ca1d13364b88feaa15477f85c2d67a53
ankle-code <lucaspasseli.work@gmail.com>
2021-05-21 15:12:18 -0300

    ✨ feat: Add Badge component

---
+++ src/components/Badges/index.js
@@ -0,0 +1,9 @@
+import * as S from "./style";
+
+const Badge = ({ name }) => (
+  <S.Container>
+    <S.Text>{name}</S.Text>
+  </S.Container>
+);
+
+export default Badge;
---
+++ src/components/Badges/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: 1px solid rgb(0, 0, 0, 0.1);
+  padding: 0.2rem;
+  border-radius: 0.2rem;
+  display: inline;
+  width: fit-content;
+`;
+
+export const Text = styled.span`
+  font-size: 0.8rem;
+  color: var(--gray);
+`;
