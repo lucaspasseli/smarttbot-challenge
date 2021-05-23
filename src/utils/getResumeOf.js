ee62734e5e98b83c20d90079e682e789a460c204
ankle-code <lucaspasseli.work@gmail.com>
2021-05-23 01:04:17 -0300

    🔥 fix: Remove getResumeOf function

--- src/utils/getResumeOf.js
+++
@@ -1,5 +0,0 @@
-export const getResumeOF = (papers) => {
-  const resume = papers.reduce((resume, { profit }) => resume + profit, 0);
-  const resumeFormated = Number(resume.toFixed(2));
-  return resumeFormated;
-};
