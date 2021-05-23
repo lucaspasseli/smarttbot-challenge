3c4e4cbe47c76a812c627a0c3cf3c03a53292506
ankle-code <lucaspasseli.work@gmail.com>
2021-05-23 03:29:54 -0300

    ✨ feat: Add a conditional render to PaperInfo

--- src/components/PaperInfo/index.js
+++ src/components/PaperInfo/index.js
@@ -3,22 +3,30 @@ import { IoMdArrowDropup, IoMdArrowDropdown } from "react-icons/io";
 
 const PaperInfo = ({ paper, paperValue, position, profit, type }) => (
   <S.Container>
-    <S.Position>{position}</S.Position>
-    <S.Wrap>
+    {(paper && paperValue && position && profit && type && (
+      <>
+        <S.Position>{position}</S.Position>
+        <S.Wrap>
+          <S.Text weight="500" color="gray">
+            {paper}
+          </S.Text>
+          <S.Text>{type ? "Compra" : "Venda"}</S.Text>
+        </S.Wrap>
+        <div>
+          <S.Text weight="300" color="gray">
+            {paperValue}
+          </S.Text>
+          <S.Text color={profit >= 0 ? "green" : "orange"} isMoney>
+            {profit >= 0 ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+            R${profit}
+          </S.Text>
+        </div>
+      </>
+    )) || (
       <S.Text weight="500" color="gray">
-        {paper}
+        Último papel não encontrado
       </S.Text>
-      <S.Text>{type ? "Compra" : "Venda"}</S.Text>
-    </S.Wrap>
-    <div>
-      <S.Text weight="300" color="gray">
-        {paperValue}
-      </S.Text>
-      <S.Text color="green" isMoney>
-        <IoMdArrowDropdown />
-        R${profit}
-      </S.Text>
-    </div>
+    )}
   </S.Container>
 );
 
