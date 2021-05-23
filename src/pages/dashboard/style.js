03dee85832376084f1132384e647eade632cbe62
ankle-code <lucaspasseli.work@gmail.com>
2021-05-23 02:39:05 -0300

    💄 fix: Adjusts Dashboard grid

--- src/pages/dashboard/style.js
+++ src/pages/dashboard/style.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   padding: 1.5rem;
   display: grid;
   grid-template-columns: 4vw auto;
-  grid-template-rows: min(4rem) 1fr min(5.5rem) 1fr;
+  grid-template-rows: min(4rem) auto min(5.5rem) 1fr;
   gap: 1rem 2rem;
   grid-template-areas:
     "SideBar Header"
