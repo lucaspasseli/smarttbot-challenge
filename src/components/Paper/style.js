8551820b11d8c49b3fed8029f3960fcd07fe0b11
ankle-code <lucaspasseli.work@gmail.com>
2021-05-24 03:07:43 -0300

    🐛 fix: Adjusts Paper component toggle behavior

--- src/components/Paper/style.js
+++ src/components/Paper/style.js
@@ -2,20 +2,12 @@ import isToday from "date-fns/isToday";
 import styled from "styled-components";
 
 export const Paper = styled.div`
-  width: ${({ width }) => width || "100%"};
-  height: ${({ height }) => height || "auto"};
+  max-width: 100%;
+  height: 100%;
   border-radius: 8px;
 
   background-color: var(--white);
   box-shadow: 0px 0px 1px 0.1px rgba(181, 180, 182, 0.85);
-  position: ${({ position }) => position && "fixed"};
   grid-area: ${({ gridArea }) => gridArea};
   cursor: ${({ isToggle }) => isToggle && "pointer"};
-  transition: 0.4s;
-
-  &:hover {
-    width: ${({ isToggle }) => isToggle && "13rem"};
-    z-index: ${({ isToggle }) => isToggle && "1"};
-    /* transition: 0.4s; */
-  }
 `;
