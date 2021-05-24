e0b7e05f8c0757ac6cba70d4328b0bc7ea3f66d7
ankle-code <lucaspasseli.work@gmail.com>
2021-05-24 02:51:23 -0300

    💄 feat: Add a responsive stylization to SideBar

--- src/components/SideBar/style.js
+++ src/components/SideBar/style.js
@@ -1,9 +1,34 @@
 import styled from "styled-components";
 
-export const Sidebar = styled.div`
+export const SideBar = styled.div`
+  width: 100%;
+  transition: 0.4s;
+  z-index: 1;
+
+  @media screen and (min-width: 795px) {
+    position: fixed;
+    width: 4rem;
+    height: calc(100vh - 3rem);
+
+    &:hover {
+      width: 13rem;
+      transition: 0.4s;
+    }
+  }
+`;
+
+export const Wrap = styled.div`
+  height: 100%;
+  width: 90%;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
-  padding: 2rem 0;
+  margin: 0 2rem;
   gap: 2rem;
+
+  @media screen and (min-width: 795px) {
+    flex-direction: column;
+    padding: 2rem 0;
+    margin: 0;
+  }
 `;
