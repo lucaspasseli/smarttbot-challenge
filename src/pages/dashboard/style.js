dfdede96a3e01301c3317cc0e5cf993269752a9b
ankle-code <lucaspasseli.work@gmail.com>
2021-05-24 02:48:37 -0300

    💄 feat: Add a responsive stylization to Dashboard

--- src/pages/dashboard/style.js
+++ src/pages/dashboard/style.js
@@ -5,12 +5,24 @@ export const Container = styled.div`
   background-color: var(--gray-light);
   padding: 1.5rem;
   display: grid;
-  grid-template-columns: 4vw auto;
-  grid-template-rows: min(4rem) auto min(5.5rem) 1fr;
+  grid-template-columns: 1fr;
+  grid-template-rows: 4rem min(4rem) auto min(13rem) auto;
   gap: 1rem 2rem;
   grid-template-areas:
-    "SideBar Header"
-    "SideBar Operations"
-    "SideBar AddBot"
-    "SideBar CardContainer";
+    "SideBar"
+    " Header"
+    " Operations"
+    " AddBot"
+    " CardContainer";
+
+  @media screen and (min-width: 795px) {
+    grid-template-columns: 4rem auto;
+    grid-template-rows: min(4rem) auto min(5.5rem) 1fr;
+    gap: 1rem 2rem;
+    grid-template-areas:
+      "SideBar Header"
+      "SideBar Operations"
+      "SideBar AddBot"
+      "SideBar CardContainer";
+  }
 `;
