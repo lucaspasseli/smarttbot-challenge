2331b3150c56a4fbe81eee86b35bb4e3f3eefec9
ankle-code <lucaspasseli.work@gmail.com>
2021-05-24 02:50:52 -0300

    💄 feat: Add a responsive stylization to Status

--- src/components/Status/style.js
+++ src/components/Status/style.js
@@ -2,7 +2,14 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
   gap: 0.5rem;
+  margin-top: 1rem;
+
+  @media screen and (min-width: 340px) {
+    margin: 0;
+  }
 `;
 
 export const Circle = styled.div`
