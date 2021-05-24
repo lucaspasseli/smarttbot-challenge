3cc0863eb5515e4716cbb51ea651b987b98d91d6
ankle-code <lucaspasseli.work@gmail.com>
2021-05-24 09:04:27 -0300

    ✨ feat: Add dispatchs in Dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -4,15 +4,28 @@ import Operations from "../../components/Operations";
 import * as S from "./style";
 import AddBot from "../../components/AddBot";
 import CardContainer from "../../components/Cards/CardContainer";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getOverviewThunk } from "../../store/modules/overview/thunks";
+import { getRobotsThunk } from "../../store/modules/robots/thunks";
 
-const Dashboard = () => (
-  <S.Container>
-    <SideBar />
-    <Header />
-    <Operations />
-    <AddBot />
-    <CardContainer />
-  </S.Container>
-);
+const Dashboard = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getOverviewThunk());
+    dispatch(getRobotsThunk());
+  }, []);
+
+  return (
+    <S.Container>
+      <SideBar />
+      <Header />
+      <Operations overview={useSelector(({ overview }) => overview)} />
+      <AddBot />
+      <CardContainer robots={useSelector(({ robots }) => robots)} />
+    </S.Container>
+  );
+};
 
 export default Dashboard;
